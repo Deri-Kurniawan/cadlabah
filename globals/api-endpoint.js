f173634b564c7c04d1e1adb222d0277201e01a8e
Deri Kurniawan <deri.netuchi@gmail.com>
2021-11-29 20:53:05 +0700

    adding APIs endpoint

---
+++ globals/api-endpoint.js
@@ -0,0 +1,16 @@
+const { BASE_URL_API } = require('./config');
+
+const API_ENDPOINT = {
+  getUsers: `${BASE_URL_API}users`,
+  getUser: (id) => `${BASE_URL_API}users/${id}`,
+  postUser: () => `${BASE_URL_API}users`,
+  putUser: (id) => `${BASE_URL_API}users/${id}`,
+  deleteUser: (id) => `${BASE_URL_API}users/${id}`,
+  getPosts: `${BASE_URL_API}posts`,
+  getPost: (id) => `${BASE_URL_API}posts/${id}`,
+  postPost: () => `${BASE_URL_API}posts`,
+  putPost: (id) => `${BASE_URL_API}posts/${id}`,
+  deletePost: (id) => `${BASE_URL_API}posts/${id}`,
+};
+
+module.exports = API_ENDPOINT;
