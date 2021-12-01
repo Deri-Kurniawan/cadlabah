399674a76c1eb6283293a49513359d36f88bb79b
Deri Kurniawan <deri.netuchi@gmail.com>
2021-12-01 18:11:38 +0700

    adding onscroll change color

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,17 @@
-console.log('Hello World!');
+const navbar = document.querySelector('#navbar');
+const buttonLogInToggler = document.querySelector('#buttonLogInToggler');
+
+window.addEventListener('scroll', () => {
+  console.log(window.scrollY);
+  if (window.scrollY > 20) {
+    navbar.classList.replace('navbar-dark', 'navbar-light');
+    navbar.classList.replace('bg-primary', 'bg-light');
+    navbar.classList.add('shadow');
+    buttonLogInToggler.classList.replace('btn-primary', 'btn-outline-primary');
+  } else {
+    navbar.classList.replace('navbar-light', 'navbar-dark');
+    navbar.classList.replace('bg-light', 'bg-primary');
+    navbar.classList.remove('shadow');
+    buttonLogInToggler.classList.replace('btn-outline-primary', 'btn-primary');
+  }
+});
