572a015de465af918c9b69ddf9c2b2c76fc1f8c8
Deri Kurniawan <deri.netuchi@gmail.com>
2021-12-01 18:02:39 +0700

    adding color changer on event scrolling

---
+++ public/js/home.js
@@ -0,0 +1,17 @@
+const navbar = document.querySelector('#navbar');
+const buttonLogInToggler = document.querySelector('#buttonLogInToggler');
+
+window.addEventListener('scroll', () => {
+  console.log(window.scrollY);
+  if (window.scrollY > 75) {
+    navbar.classList.replace('navbar-dark', 'navbar-light');
+    navbar.classList.replace('bg-primary', 'bg-light');
+    navbar.classList.add('shadow');
+    buttonLogInToggler.classList.replace('btn-primary', 'btn-outline-primary');
+  } else {
+    navbar.classList.replace('navbar-light', 'navbar-dark');
+    navbar.classList.replace('bg-light', 'bg-primary');
+    navbar.classList.remove('shadow');
+    buttonLogInToggler.classList.replace('btn-outline-primary', 'btn-primary');
+  }
+});
