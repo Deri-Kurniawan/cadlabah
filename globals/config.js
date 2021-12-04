1748ed20cfba389855839f8703f33bb2540bb074
Deri Kurniawan <deri.netuchi@gmail.com>
2021-12-04 20:42:33 +0700

    add prod and dev mode

--- globals/config.js
+++ globals/config.js
@@ -1,7 +1,7 @@
-const PORT = 9000;
+const PORT = process.env.SERVER_PORT || 8000;
 
 const CONFIG = {
-  BASE_URL: `http://localhost:${PORT}/`,
+  BASE_URL: (process.env.MODE === 'production') ? 'https://cadlabah.glitch.me/' : `http://localhost:${PORT}/`,
   BASE_URL_API: 'https://619b71a827827600174455d9.mockapi.io/api/',
   SERVER_PORT: PORT,
 };
