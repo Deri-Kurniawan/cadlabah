8c0692b82326356d819e7881cbcd6a2e3dbac497
Deri Kurniawan <deri.netuchi@gmail.com>
2021-12-14 13:44:25 +0700

    Optimize Web Performance

--- app/helpers/mail-helper.js
+++ app/helpers/mail-helper.js
@@ -26,12 +26,12 @@ const sendGreetingsFirstTime = async (transporter, { fullName, to }) => {
     subject: `Welcome to Cadlabah ${fullName}!`,
     text: `
     <p>Hai, ${fullName}!. Kami Sangat senang karena anda telah bergabung pada platform kami.</p>
-    <p>Mari mulai jelajahi barang yang hilang ataupun yang ditemukan di <a href="${BASE_URL}posts" target="_blank">sini</a></p>
+    <p>Mari mulai jelajahi barang yang hilang ataupun yang ditemukan di <a href="${BASE_URL}posts" target="_blank" rel="noopener">sini</a></p>
     butuh bantuan? <a href="mailto:cadlabahlimited@gmail.com">Tanya melalui email</a>
     `,
     html: `
     <p>Hai, ${fullName}!. Kami Sangat senang karena anda telah bergabung pada platform kami.</p>
-    <p>Mari mulai jelajahi barang yang hilang ataupun yang ditemukan di <a href="${BASE_URL}posts" target="_blank">sini</a></p>
+    <p>Mari mulai jelajahi barang yang hilang ataupun yang ditemukan di <a href="${BASE_URL}posts" target="_blank" rel="noopener">sini</a></p>
     butuh bantuan? <a href="mailto:cadlabahlimited@gmail.com">Tanya melalui email</a>
     `,
   });
