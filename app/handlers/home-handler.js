99d7f2c431932745d3ea1dbeffa322b375435d17
Deri Kurniawan <deri.netuchi@gmail.com>
2022-04-30 19:49:57 +0700

    add dynamic alanytics

--- app/handlers/home-handler.js
+++ app/handlers/home-handler.js
@@ -1,11 +1,27 @@
-const homeHandler = (req, res) => {
-  res.render('home', {
-    title: 'Home',
-    user: req.user,
-    usersTotal: '1.000',
-    postsTotal: '2.000',
-    helpedTotal: '800',
-    notif: req.flash('notif'),
+const { getPosts } = require('../models/posts-model');
+const { getUsers } = require('../models/users-model');
+
+const homeHandler = async (req, res) => {
+  let usersTotal = 0;
+  let postsTotal = 0;
+  let helpedTotal = 0;
+
+  getUsers((users) => {
+    usersTotal = users.length;
+
+    getPosts((posts) => {
+      postsTotal = posts.length;
+      helpedTotal = posts.filter((post) => post.isDone === true).length;
+
+      res.render('home', {
+        title: 'Home',
+        user: req.user,
+        usersTotal,
+        postsTotal,
+        helpedTotal,
+        notif: req.flash('notif'),
+      });
+    });
   });
 };
 
